f3fbcae8d917b9fac9682d63735ff45d84f7bf9d
Peter Shkenev <peter.shhh@yandex.ru>
2023-10-04 14:47:01 +0000

    Main: introduce --no-gui option

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include "GUI/main_window_kernel.h"
 
 #include <QApplication>
+#include <QScopedPointer>
 #include <QTextCodec>
+
+bool noGui(int argc, char* argv[]) {
+  for (char **arg = argv; *arg; arg++)
+    if (!qstrcmp(*arg, "--no-gui"))
+      return true;
+  return false;
+}
 
 int main(int argc, char* argv[]) {
   // Установка кодировки UTF-8 для консоли вывода
+#elif _WIN32
   QTextCodec::setCodecForLocale(QTextCodec::codecForName("CP1251"));
+#endif /* _WIN32 */
 
-  QApplication a(argc, argv);
-  MainWindowKernel w;
-  w.show();
-  return a.exec();
+  if (noGui(argc, argv)) {
+    QCoreApplication app(argc, argv);
+    return app.exec();
+  }
+  else {
+    QApplication app(argc, argv);
+    MainWindowKernel w;
+    w.show();
+    return app.exec()
+  }
 }
